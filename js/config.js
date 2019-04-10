9053c2adbe2817a05eead119801b72cea427645c
Tao Jin (Stephen) <jin.hybr@gmail.com>
2019-04-10 16:01:01 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ";
+const API_KEY = "pk.eyJ1IjoiamluYmlsbHMiLCJhIjoiY2p1NGZtNm9hMHhqazQ1cDhybXh2Mjl6MCJ9.eq6bgxgDo5BkzFsppZoFsg";
